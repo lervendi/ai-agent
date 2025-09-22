b1bdaf995adf5113b5de13b0c42af1cb17693c91
lervendi <lervendi111@gmail.com>
2025-09-22 18:29:36 +0100

    feat(verbose): add verbose flag to show user promt and token counts

--- main.py
+++ main.py
@@ -1,23 +1,39 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-
+# Load environment variables from .env file
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
-
 client = genai.Client(api_key=api_key)
 
-responce = client.models.generate_content(
-    model="gemini-2.0-flash-001", 
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    )
 
 def main():
-    print("Hello from ai-agent!")
-    print(responce.text)
-    print(f"Prompt tokens: {responce.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {responce.usage_metadata.candidates_token_count}")
+    if len(sys.argv) < 2:
+        print("Error: Please provide a prompt as a command-line argument.")
+        sys.exit(1)
+
+    user_prompt = sys.argv[1]
+
+    verbose = "--verbose" in sys.argv[2:]
+
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", 
+        contents=messages
+    )
+
+    print(response.text)
+
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
